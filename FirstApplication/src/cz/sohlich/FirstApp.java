e031edfba57683d3225b8c920092d1007a5f91f8
Radomir Sohlich <sohlich@gmail.com>
2015-10-13 06:19:21 +0200

    Non maven projects.

---
+++ FirstApplication/src/cz/sohlich/FirstApp.java
@@ -0,0 +1,16 @@
+package cz.sohlich;
+
+/**
+ * The application should ask for your credentials and after it obtains all
+ * information, it just prints out the information in formatted output.
+ *
+ *
+ * @author Radomir Sohlich
+ */
+public class FirstApp {
+
+    public static void main(String[] args) {
+
+    }
+
+}
